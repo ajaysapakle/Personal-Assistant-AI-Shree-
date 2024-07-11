df909e41762a87450eb202a289e806f9504163d8
Ajay Rajendra  Sapkale <157692595+ajaysapakle@users.noreply.github.com>
2024-07-11 15:29:02 +0530

    Personal Assistant AI(Shree)

---
+++ Clap.py
@@ -0,0 +1,29 @@
+#import pyaudio
+import sounddevice as sd
+import numpy as np 
+
+threshold = 20
+Clap = False
+
+def detect_clap(indata,frames,time,status):
+    global Clap 
+    volume_norm = np.linalg.norm(indata) * 10
+    if volume_norm>threshold:
+        print("Clapped")
+        Clap = True
+    
+def Listen_for_claps():
+    with sd.InputStream(callback=detect_clap):
+        return sd.sleep(1000)
+
+def MainClapExe():
+
+#if __name__ == "__main__":
+
+    while True:
+        Listen_for_claps()
+        if Clap==True:
+            break
+
+        else:
+            pass
